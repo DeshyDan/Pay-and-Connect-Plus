85c7e38419e6c0ad44102998168ea02a269497fa
Deshy Dan <deshy733@gmail.com>
2023-09-30 12:07:18 +0200

    Added the Crawler class

---
+++ src/main/java/payandconnectplus/Crawler.java
@@ -0,0 +1,37 @@
+package payandconnectplus;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Crawler {
+    private WebDriver driver;
+    private String studentNumber;
+    private String password;
+
+    public Crawler(String studentNumber , String password){
+
+        this.driver = new ChromeDriver();
+        this.studentNumber = studentNumber;
+        this.password = password;
+        driver.get("https://app.payandconnect.co.za/login");
+    }
+
+    public void run(){
+    signIn();
+    }
+
+    private void signIn(){
+        WebElement studentNumberInput = driver.findElement(By.id("user"));
+        WebElement passwordInput = driver.findElement(By.id("password"));
+        WebElement signingButton = driver.findElement(By.id("submit-button"));
+
+        studentNumberInput.sendKeys(studentNumber);
+        passwordInput.sendKeys(password);
+        signingButton.click();
+
+
+
+    }
+}
